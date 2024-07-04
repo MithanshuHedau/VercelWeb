0c3a4313b9dfa53986514eecf788fdabc189750c
mithanshu hedau <hedaumithanshu@gmail.com>
2024-07-04 12:53:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p> <b>Mithanshu's </b> Sample Vercel Website</p>
+      <img src="https://images.unsplash.com/photo-1620712943543-bcc4688e7485?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWklMjBpYW1nZXxlbnwwfHwwfHx8MA%3D%3D" height={"400px"} alt="" />
+    </div>
+  )
+}
+
+export default App
